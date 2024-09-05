4222c150765dfd793cf41f6b56c87f0d992bd671
LENOVO <michaeltkd14@gmail.com>
2024-09-05 16:51:41 -0600

    show api info

--- src/app/products/features/product-list/product-list.component.ts
+++ src/app/products/features/product-list/product-list.component.ts
@@ -1,12 +1,13 @@
 import { Component ,inject} from '@angular/core';
 import { ProductsSateService } from '../../data-access/products-state.service';
+import { ProductCardComponent } from '../../ui/product-card/product-card.component';
 
 
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
   styles: ``,
   providers:[ProductsSateService],
---
+++ src/app/products/ui/product-card/product-card.component.ts
@@ -0,0 +1,12 @@
+import { Component ,input} from '@angular/core';
+import { Product } from '../../../shared/interfaces/product.interface';
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styles: ``
+})
+export class ProductCardComponent {
+   product = input.required<Product>()
+}
